5ff4b287424c4989eb466b7c2a72ca78050de11f
Miso Korkiakoski <miso.korkiakoski@gmail.com>
2011-03-22 20:33:12 +0200

    add default handler
    Signed-off-by: Miso Korkiakoski <miso.korkiakoski@gmail.com>

--- scala/src/main/scala/fi/reaktor/MongoFilter.scala
+++ scala/src/main/scala/fi/reaktor/MongoFilter.scala
@@ -9,6 +9,15 @@ import java.net.URL
 class MongoFilter extends ScalatraFilter {
 
   get("/") {
-    "{}"
+    <html><head><title>mongo-koulutus</title></head><body><h4>Not implemented</h4></body></html>
+  }
+
+ notFound {
+    response.setStatus(404)
+    <html>
+      <body>
+        <h1>404 Not Found</h1>
+      </body>
+    </html>
   }
 }
