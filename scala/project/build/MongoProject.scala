afb9f270efda3afb7dc057fc8d49accbf799442a
Miso Korkiakoski <miso.korkiakoski@gmail.com>
2011-03-22 20:48:46 +0200

    include casbah
    Signed-off-by: Miso Korkiakoski <miso.korkiakoski@gmail.com>

--- scala/project/build/MongoProject.scala
+++ scala/project/build/MongoProject.scala
@@ -1,10 +1,18 @@
 import sbt._
 
 class MongoProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val scalatraVersion = "2.0.0.M2"
+  val scalatraVersion = "2.0.0.M3"
   val scalatra = "org.scalatra" %% "scalatra" % scalatraVersion
   val servletApi = "org.mortbay.jetty" % "servlet-api" % "2.5-20081211" % "provided"
+  
+  // Json
   val liftJson = "net.liftweb" %% "lift-json" % "2.2"
+  
+  // Casbah repos and dependency
+  val bumRels = "bum-releases" at "http://repo.bumnetworks.com/releases" 
+  val bumSnaps = "bum-snapshots" at "http://repo.bumnetworks.com/snapshots" 
+  val casbah = "com.novus" % "casbah_2.8.0" % "1.0.8.5" 
+  
   val scalatest = "org.scalatra" %% "scalatra-scalatest" % scalatraVersion % "test"
   val slf4jBinding = "ch.qos.logback" % "logback-classic" % "0.9.25" % "runtime"
   override def testClasspath = super.testClasspath +++ buildCompilerJar
