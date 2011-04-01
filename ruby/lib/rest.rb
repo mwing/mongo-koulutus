4df841c401eefb794a46a315c9752e26374e86f0
Miso Korkiakoski <miso.korkiakoski@gmail.com>
2011-04-01 14:02:06 +0300

    do an actual query in /venues/list
    Implemented in the ruby side
    
    Signed-off-by: Miso Korkiakoski <miso.korkiakoski@gmail.com>

--- ruby/lib/rest.rb
+++ ruby/lib/rest.rb
@@ -1,11 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require File.join(File.dirname(__FILE__), "documents.rb")
+
+MongoMapper.database = "3sq"
 
 class MongoRestApp < Sinatra::Base
   @@venues = [{:venue => "Logalialainen", :loc => [51,50]}, {:venue => "Gastone", :loc => [50,51]}]
   @@users = [{:name => "matti", :friends => ["pekka", "jaska"]}, {:name => "pekka", :friends => ["jaska"]}, {:name => "jaska", :friends => ["pekka"]}]
   get "/venues/list" do
-    @@venues.to_json
+#    @@venues.to_json
+    Venue.all.to_json
   end
 
   get "/venues/search/:term" do |search_term|
