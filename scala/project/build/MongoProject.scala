1d47fa6334b5d002a0282d248b0ce1ae8ca48ec5
Miso Korkiakoski <miso.korkiakoski@gmail.com>
2011-04-04 15:20:04 +0300

    implement basic mongo queries for venues
    Venues searches:
    * List all
    * query by name
    * find venues near a location
    
    Signed-off-by: Miso Korkiakoski <miso.korkiakoski@gmail.com>

--- scala/project/build/MongoProject.scala
+++ scala/project/build/MongoProject.scala
@@ -7,15 +7,19 @@ class MongoProject(info: ProjectInfo) extends DefaultWebProject(info) {
   
   // Json
   val liftJson = "net.liftweb" %% "lift-json" % "2.2"
-  
-  // Casbah repos and dependency
-  val bumRels = "bum-releases" at "http://repo.bumnetworks.com/releases" 
-  val bumSnaps = "bum-snapshots" at "http://repo.bumnetworks.com/snapshots" 
-  val casbah = "com.novus" % "casbah_2.8.0" % "1.0.8.5" 
-  val rogue = "com.foursquare" % "rogue_2.8.0" % "1.0.3" withSources()  
+ 
+  val slf4jBinding = "ch.qos.logback" % "logback-classic" % "0.9.28" % "runtime"
+
+  val casbah = "com.novus" % "casbah_2.8.0" % "1.0.8.5"
+//  val rogue = "com.foursquare" % "rogue_2.8.0" % "1.0.3" withSources()
   val scalatest = "org.scalatra" %% "scalatra-scalatest" % scalatraVersion % "test"
-  val slf4jBinding = "ch.qos.logback" % "logback-classic" % "0.9.25" % "runtime"
+
   override def testClasspath = super.testClasspath +++ buildCompilerJar
+
   val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
   val fuseSourceSnapshots = "FuseSource Snapshot Repository" at "http://repo.fusesource.com/nexus/content/repositories/snapshots"
+  val stRels = "scalatools-releases" at "http://scala-tools.org/repo-releases"
+  val stSnaps = "scalatools-snapshots" at "http://scala-tools.org/repo-snapshots"
+  val bumRels = "bum-releases" at "http://repo.bumnetworks.com/releases"
+  val bumSnaps = "bum-snapshots" at "http://repo.bumnetworks.com/snapshots"
 }
--- scala/src/main/scala/fi/reaktor/MongoFilter.scala
+++ scala/src/main/scala/fi/reaktor/MongoFilter.scala
@@ -1,23 +1,78 @@
 package fi.reaktor
 
 import org.scalatra._
-import net.liftweb.json.JsonAST
-import net.liftweb.json.JsonDSL
-import net.liftweb.json.JsonParser
-import java.net.URL
+import net.liftweb.json._
+import net.liftweb.json.Extraction._
+import net.liftweb.json.Printer._
+import net.liftweb.json.JsonAST._
+import net.liftweb.json.JsonDSL._
+import net.liftweb.json.Serialization.{read, write}
+import com.mongodb.{BasicDBObject, BasicDBList}
+import com.novus.casbah.mongodb.{MongoDBObject, MongoConnection}
 
 class MongoFilter extends ScalatraFilter {
 
+  implicit val formats = Serialization.formats(NoTypeHints) + new VenueSerializer
+  val venues = MongoConnection()("3sq")("venues")
+
   get("/") {
-    <html><head><title>mongo-koulutus</title></head><body><h4>Not implemented</h4></body></html>
+    <html>
+      <head>
+        <title>mongo-koulutus</title>
+      </head> <body>
+      <h4>Not implemented</h4>
+    </body>
+    </html>
   }
 
- notFound {
+  get("/venues/list") {
+    val all = for (x <- venues.find()) yield read[Venue](x.toString)
+    write(all.toList)
+  }
+
+  get("/venues/search/:venue") {
+    val query = MongoDBObject("name" -> params("venue").r)
+    val results = for (x <- venues.find(query)) yield read[Venue](x.toString)
+    write(results.toList)
+  }
+
+  get("/venues/near/:lat/:long") {
+    val location = List(params("lat").toDouble, params("long").toDouble)
+    val q = MongoDBObject("location" -> MongoDBObject("$near" -> location, "$maxDistance" -> 0.01))  // maxDistance: 1 ~ 70km
+    val results = for (x <- venues.find(q)) yield read[Venue](x.toString)
+    write(results.toList)
+  }
+
+  notFound {
     response.setStatus(404)
     <html>
       <body>
         <h1>404 Not Found</h1>
       </body>
     </html>
+  }
+}
+case class Location(lat: Double, long: Double)
+case class Venue(id: String, name: String, location: Location)
+
+class VenueSerializer extends Serializer[Venue] {
+  private val venueClass = classOf[Venue]
+
+  def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Venue] = {
+    case (TypeInfo(venueClass, _), json) => json match {
+      case JObject( JField("_id", JObject(JField("$oid", JString(id)) :: Nil)) ::
+                    JField("name", JString(name)) ::
+                    JField("location", JArray(List(JDouble(lat), JDouble(long)))) ::
+                    Nil) =>
+        new Venue(id, name, Location(lat, long))
+      case x => throw new MappingException("Can't convert " + x + " to Venue")
+    }
+  }
+
+  def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
+    case x: Venue =>
+      JObject(JField("_id", JObject(JField("$oid", JString(x.id)) :: Nil)) ::
+        JField("name", JString(x.name)) ::
+        JField("location", JArray(List(JDouble(x.location.lat), JDouble(x.location.long)))) :: Nil)
   }
 }
