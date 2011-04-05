706e12282bb60b4504ca8501c918607ec5fc5122
Miso Korkiakoski <miso.korkiakoski@gmail.com>
2011-04-05 13:17:25 +0300

    Change to Rogue coz it's cool
    Signed-off-by: Miso Korkiakoski <miso.korkiakoski@gmail.com>

--- scala/src/main/scala/fi/reaktor/MongoFilter.scala
+++ scala/src/main/scala/fi/reaktor/MongoFilter.scala
@@ -2,48 +2,46 @@ package fi.reaktor
 
 import org.scalatra._
 import net.liftweb.json._
-import net.liftweb.json.Serialization.{read, write}
-import com.novus.casbah.mongodb.{MongoDBObject, MongoConnection}
+import net.liftweb.json.Serialization.write
+import net.liftweb.mongodb._
+import com.foursquare.rogue.Rogue._
+import com.foursquare.rogue.Degrees
+import java.util.regex.Pattern
 
 class MongoFilter extends ScalatraFilter {
 
   implicit val formats = Serialization.formats(NoTypeHints) + new VenueSerializer
-  val venues = MongoConnection()("3sq")("venues")
+  MongoDB.defineDb(ThreeSquareMongoIdentifier, MongoAddress(MongoHost("127.0.0.1", 27017), "3sq"))
+
+  get("/venues/list") {
+    write(Venue.fetch())
+  }
+
+  get("/venues/search/:name") {
+    val all = Venue where(_.name regexWarningNotIndexed Pattern.compile(params("name")))
+    write(all.fetch)
+  }
+
+  get("/venues/near/:lat/:long") {
+    val lat = params("lat").toLong
+    val long = params("long").toLong
+    val distance = Degrees(1)
+    val q = Venue where(_.location near (lat, long, distance))
+    write(q.fetch)
+  }
 
   get("/") {
     <html>
       <head>
         <title>mongo-koulutus</title>
       </head> <body>
-      <h4>Not implemented</h4>
+      <h4>Urls</h4>
+      <ul>
+         <li><a href="/venues/list">/venues/list</a></li>
+         <li><a href="/venues/search/Zorbas">/venues/search/(part of venue name)</a></li>
+         <li><a href="/venues/near/24/60">/venues/near/(latitude)/(longtitude)</a></li>
+      </ul>
     </body>
-    </html>
-  }
-
-  get("/venues/list") {
-    val all = for (x <- venues.find()) yield read[Venue](x.toString)
-    write(all.toList)
-  }
-
-  get("/venues/search/:venue") {
-    val query = MongoDBObject("name" -> params("venue").r)
-    val results = for (x <- venues.find(query)) yield read[Venue](x.toString)
-    write(results.toList)
-  }
-
-  get("/venues/near/:lat/:long") {
-    val location = List(params("lat").toDouble, params("long").toDouble)
-    val q = MongoDBObject("location" -> MongoDBObject("$near" -> location, "$maxDistance" -> 0.1))  // maxDistance: 1 ~ 70km
-    val results = for (x <- venues.find(q)) yield read[Venue](x.toString)
-    write(results.toList)
-  }
-
-  notFound {
-    response.setStatus(404)
-    <html>
-      <body>
-        <h1>404 Not Found</h1>
-      </body>
     </html>
   }
 }
--- scala/src/main/scala/fi/reaktor/Venue.scala
+++ scala/src/main/scala/fi/reaktor/Venue.scala
@@ -2,29 +2,40 @@ package fi.reaktor
 
 import net.liftweb.json._
 import net.liftweb.json.JsonAST._
-
-
-case class Location(lat: Double, long: Double)
-case class Venue(id: String, name: String, location: Location)
+import net.liftweb.record.field.StringField
+import net.liftweb.mongodb.record.{MongoMetaRecord, MongoId, MongoRecord}
+import net.liftweb.mongodb.MongoIdentifier
+import com.foursquare.rogue.LatLong
+import net.liftweb.mongodb.record.field.MongoCaseClassField
 
 class VenueSerializer extends Serializer[Venue] {
   private val venueClass = classOf[Venue]
 
   def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Venue] = {
     case (TypeInfo(venueClass, _), json) => json match {
-      case JObject( JField("_id", JObject(JField("$oid", JString(id)) :: Nil)) ::
+      case JObject( JField("_id", JString(id)) ::
                     JField("name", JString(name)) ::
                     JField("location", JArray(List(JDouble(lat), JDouble(long)))) ::
-                    Nil) =>
-        new Venue(id, name, Location(lat, long))
+                    Nil) => Venue
       case x => throw new MappingException("Can't convert " + x + " to Venue")
     }
   }
 
   def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
-    case x: Venue =>
-      JObject(JField("_id", JObject(JField("$oid", JString(x.id)) :: Nil)) ::
-        JField("name", JString(x.name)) ::
-        JField("location", JArray(List(JDouble(x.location.lat), JDouble(x.location.long)))) :: Nil)
+    case x: Venue => x.asJValue
   }
+}
+
+class Venue extends MongoRecord[Venue] with MongoId[Venue] {
+  def meta = Venue
+  object name extends StringField(this, 255)
+  object location extends MongoCaseClassField[Venue, LatLong](this)
+}
+
+object ThreeSquareMongoIdentifier extends MongoIdentifier {
+  override val jndiName = "3sq"
+}
+
+object Venue extends Venue with MongoMetaRecord[Venue] {
+  override def mongoIdentifier = ThreeSquareMongoIdentifier
 }
