05e9aebced092df2e9d94226f68ccfb9ed8cc6ad
Miso Korkiakoski <miso.korkiakoski@reaktor.fi>
2011-04-12 21:58:34 +0300

    Say goodbye to the Serializers
    Thanks @jonifreeman for the help
    
    Signed-off-by: Miso Korkiakoski <miso.korkiakoski@reaktor.fi>

--- scala/src/main/scala/fi/reaktor/MongoFilter.scala
+++ scala/src/main/scala/fi/reaktor/MongoFilter.scala
@@ -2,15 +2,14 @@ package fi.reaktor
 
 import org.scalatra._
 import net.liftweb.json._
-import net.liftweb.json.Serialization.write
 import net.liftweb.mongodb._
 import com.foursquare.rogue.Rogue._
 import com.foursquare.rogue.Degrees
 import java.util.regex.Pattern
 import com.mongodb.DBAddress
+import net.liftweb.mongodb.record.MongoRecord 
 
 class MongoFilter extends ScalatraFilter {
-  implicit val formats = Serialization.formats(NoTypeHints) + new VenueSerializer + new UserSerializer
 
   // Use this for single instance mongo
   MongoDB.defineDb(ThreeSquareMongoIdentifier, MongoAddress(MongoHost("127.0.0.1", 27017), "3sq"))
@@ -20,7 +19,7 @@ class MongoFilter extends ScalatraFilter {
   //MongoDB.defineDb(ThreeSquareMongoIdentifier, MongoAddress(replicaSet, "3sq"))
 
   get("/users/list") {
-    write(User.fetch)
+   renderObjects(User.fetch)
   }
 
   get("/friends/:user/list") {
@@ -40,12 +39,12 @@ class MongoFilter extends ScalatraFilter {
   }
 
   get("/venues/list") {
-    write(Venue.fetch)
+    renderObjects(Venue.fetch)
   }
 
   get("/venues/search/:name") {
     val all = Venue where(_.name regexWarningNotIndexed Pattern.compile(params("name")))
-    write(all.fetch)
+    renderObjects(all.fetch)
   }
 
   get("/venues/near/:lat/:long") {
@@ -53,7 +52,7 @@ class MongoFilter extends ScalatraFilter {
     val long = params("long").toLong
     val distance = Degrees(1)
     val q = Venue where(_.location near (lat, long, distance))
-    write(q.fetch)
+    renderObjects(q.fetch)
   }
 
   get("/") {
@@ -71,4 +70,6 @@ class MongoFilter extends ScalatraFilter {
     </body>
     </html>
   }
+
+ def renderObjects(objs: List[MongoRecord[_]]) = compact(render(JArray(objs.map(_.asJValue))))
 }
--- scala/src/main/scala/fi/reaktor/Venue.scala
+++ scala/src/main/scala/fi/reaktor/Venue.scala
@@ -1,35 +1,10 @@
 package fi.reaktor
 
-import net.liftweb.record.field.StringField
+import net.liftweb.record.field._
 import net.liftweb.mongodb.record.{MongoMetaRecord, MongoId, MongoRecord}
 import net.liftweb.mongodb.MongoIdentifier
 import com.foursquare.rogue.LatLong
 import net.liftweb.mongodb.record.field._
-import net.liftweb.mongodb.record.field.MongoCaseClassField
-import net.liftweb.json.JsonAST.JValue
-import net.liftweb.json.{TypeInfo, Formats, Serializer}
-
-class VenueSerializer extends Serializer[Venue] {
-  private val venueClass = classOf[Venue]
-
-  def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Venue] = {
-    case (TypeInfo(venueClass, _), json) => Venue.fromJValue(json).get
-  }
-  def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
-    case x: Venue => x.asJValue
-  }
-}
-
-class UserSerializer extends Serializer[User] {
-  private val userClass = classOf[User]
-
-  def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), User] = {
-    case (TypeInfo(userClass, _), json) => User.fromJValue(json).get
-  }
-  def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
-    case x: User => x.asJValue
-  }
-}
 
 object ThreeSquareMongoIdentifier extends MongoIdentifier {
   override val jndiName = "3sq"
